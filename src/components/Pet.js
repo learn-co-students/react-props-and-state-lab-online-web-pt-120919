1f36a3c8c94c7d93c47f492fc7327394418f33b4
Mely07 <damely.tineo07@gmail.com>
2020-08-27 02:06:02 -0400

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -2,24 +2,26 @@ import React from 'react'
 
 class Pet extends React.Component {
   render() {
+    const {pet, onAdoptPet} = this.props; 
+    //rec pet as a prop so destructuring to use as props
     return (
       <div className="card">
         <div className="content">
           <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
+            {pet.gender === 'female' ? '♀' : '♂'}
+            {pet.name}
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{pet.type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {pet.age}</p>
+            <p>Weight: {pet.weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+          {pet.isAdopted === true ? <button className="ui disabled button">Already adopted</button> : <button className="ui primary button" onClick={() => onAdoptPet(pet.id)}>Adopt pet</button> }
+           {/* when callback prop recieves arg use arrow function  */}
         </div>
       </div>
     )
